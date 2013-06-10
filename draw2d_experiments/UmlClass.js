2e9ee8d61119e91e71f4627f9a9ca52dbcec4c71
Sven Hodapp <sven.hodapp@gmail.com>
2013-06-10 17:07:54 +0200

    UmlClass extended with an additional Port. Finished drawing svg L0 0

--- draw2d_experiments/UmlClass.js
+++ draw2d_experiments/UmlClass.js
@@ -21,32 +21,45 @@ draw2d.shape.analog.UmlClass = draw2d.SVGFigure.extend({
     NAME:"draw2d.shape.analog.UmlClass",
     
     MyPortLocator1 : draw2d.layout.locator.PortLocator.extend({
-        init:function( ){
+        init:function() {
           this._super();
         },    
-        relocate:function(index, port){
-        	var w = port.getParent().getWidth();
-			var h = port.getParent().getHeight();
-			if (index == 1) {
-				this.applyConsiderRotation(port,w/2, 0);
-			} else {
-				this.applyConsiderRotation(port,w/2, h);
-			}
+        relocate:function(index, port) {
+            var w = port.getParent().getWidth();
+            var h = port.getParent().getHeight();
+            if (index == 1) {
+                this.applyConsiderRotation(port,w/2, 0);
+            } else {
+                this.applyConsiderRotation(port,w/2, h);
+            }
         }
     }),
-	
-	MyPortLocator2 : draw2d.layout.locator.PortLocator.extend({
-        init:function( ){
+
+    MyPortLocator2 : draw2d.layout.locator.PortLocator.extend({
+        init:function() {
           this._super();
         },    
-        relocate:function(index, port){
-        	var w = port.getParent().getWidth();
-			var h = port.getParent().getHeight();
-			if (index == 1) {
-				this.applyConsiderRotation(port,0, h/2);
-			} else {
-				this.applyConsiderRotation(port,w, h/2);
-			}
+        relocate:function(index, port) {
+            var w = port.getParent().getWidth();
+            var h = port.getParent().getHeight();
+            if (index == 1) {
+                this.applyConsiderRotation(port,0, h/2);
+            } else {
+                this.applyConsiderRotation(port,w, h/2);
+            }
+        }
+    }),
+
+    MyPortLocator3 : draw2d.layout.locator.PortLocator.extend({
+        init:function() {
+          this._super();
+        },    
+        relocate:function(index, port) {
+            var w = port.getParent().getWidth();
+            var h = port.getParent().getHeight();
+            this.applyConsiderRotation(port,0, 0);
+            console.log(index);
+            console.log(port);
         }
     }),
 
@@ -62,19 +75,22 @@ draw2d.shape.analog.UmlClass = draw2d.SVGFigure.extend({
         
         this._super(width,height);
         this.inputLocator = new this.MyPortLocator1();
-        this.portLocator2 = new this.MyPortLocator2(); 
+        this.portLocator2 = new this.MyPortLocator2();
+        this.portLocator3 = new this.MyPortLocator3(); 
         this.createPort("hybrid", this.inputLocator);
         this.createPort("hybrid", this.inputLocator);
         this.createPort("hybrid", this.portLocator2);
         this.createPort("hybrid", this.portLocator2);
+        this.createPort("hybrid", this.portLocator3);
+        this.createPort("hybrid", this.portLocator3);
         this.setBackgroundColor("#f0f0ff");
     },
 
     
     getSVG: function(){
          return '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">'+
-                 '<path d="M0 0 L200 0 L200 100 L0 100" stroke="#1B1B1B" fill="none"/>' +
-				 '<line x1="0"  y1="50" x2="200"  y2="50" stroke="#010101" />'+
+                  '<path d="M0 0 L200 0 L200 100 L0 100 L0 0" stroke="#1B1B1B" fill="none"/>' +
+                  '<line x1="0"  y1="50" x2="200"  y2="50" stroke="#010101" />'+
                 '</svg>';
     },
     
