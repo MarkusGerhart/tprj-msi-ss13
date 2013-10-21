03ee773cd00e8553ce5637725edda4b8c85f5270
ymc-sise <simon.schneeberger@ymc.ch>
2013-10-21 03:28:44 +0200

    implementing generator

---
+++ sprayServer/public/javascripts/spray/generator.js
@@ -0,0 +1,215 @@
+var SPRAY = SPRAY || {};
+SPRAY.htwg = SPRAY.htwg || {};
+
+SPRAY.htwg.spray = SPRAY.htwg.spray || {};
+
+// class: Generator
+// varianten: 1) konvertieren des spray json in draw2d json
+//               dann mit rekursivem unmarshall zeichnen
+//            2) direkt zeichnen von spray json und umwandlung was gebraucht?!
+
+/*shape PI_Boiler {
+    rounded-rectangle {
+        curve(width=30,height=30)
+        position (x=0, y=0)
+        size (width=60, height=100)
+    }
+    polyline {
+        point (x=90, y=70)
+        point (x=30, y=70)
+        point (x=50, y=50)
+        point (x=30, y=30)
+        point (x=90, y=30)
+    }
+    text style TextStyle {
+        position(x=60,y=0)
+        size (width=100, height=20)
+        id = shapeName
+    }
+    anchor {
+        position (x=0, y=30)
+        position (x=0, y=70)
+        position (x=90, y=30)
+        position (x=90, y=70)
+    }
+}*/
+
+
+/*
+* shape PI_Vessel_Vertical {
+     rounded-rectangle {
+         size (width=120, height=60)
+         curve (width=50, height=50)
+         text style TextStyle {
+             size (width=100, height=20)
+             position (x=3, y=20)
+             align (horizontal=center, vertical=middle)
+             id = shapeName
+         }
+     }
+ }
+*
+* */
+
+var PI_Vessel_Vertical = {
+    shapes: [{
+        name: "rounded-rectangle",
+        size: {width: 120, height: 60},
+        curve: {width: 50, height: 50},
+        shapes: [{
+            name: "text",
+            position: {x:3, y:20},
+            size: {width: 100, height: 20},
+            align: {horizontal: "center", vertical: "middle"},
+            id: "shapeName",
+            shapes: "undefined"
+        }]
+    }
+    ]
+};
+
+// to
+/*
+ var jsonDocument ={ "PI_Vessel_Vertical" : { "type": draw2d.Figure,
+                                              "id": 1,
+                                              "x": default?,
+                                              "y": default?,
+                                              children": [{
+                                                             "type": "draw2d.shape.basic.Rectangle",
+                                                             "id": "354fa3b9-a834-0221-2009-abc2d6bd852a",
+                                                             "x": 0,
+                                                             "y": 30,
+                                                             "width": 100,
+                                                             "height": 100,
+                                                             "children" : [{
+                                                                             "type": "draw2d.shape.basic.Rectangle",
+                                                                             "id": "ebfb35bb-5767-8155-c804-14bda7759dc2",
+                                                                             "locator" : "draw2d.layout.locator.LocatorA",
+                                                                             "x": 25,
+                                                                             "y": 100,
+                                                                             "width": 50,
+                                                                             "height": 20
+                                                              }]
+                                                        }]
+                                            },
+                       "PI_Vessel_2": {}
+                    };
+ */
+
+var shapeExamples = { "PI_Vessel_Vertical": PI_Vessel_Vertical };
+
+SPRAY.htwg.spray.Generator = function($){
+
+    /**
+     * Scope duplicator / parent this
+     *
+     * @var
+     * @access private
+     * @type object
+     */
+    var that = this;
+
+    this.shapeElements = [];
+    this.draw2dJSON = [];
+    this.configMenu = [];
+
+    this.initialize = function(){
+
+        this.convertSprayToDraw2dJSON(shapeExamples);
+
+        console.log(this.draw2dJSON);
+
+        $.each(this.draw2dJSON[0], function(name,elem){
+            var funcObj = that.generateFigure(elem);
+            that.shapeElements[name] = funcObj;
+
+            //TODO: probably more attributes like title
+            that.configMenu.push({"name": name})
+        });
+
+        this.configMenu.push({});
+
+        //SPRAY.htwg.menu.buildMenu( this.configMenu );
+    };
+
+    this.generateFigure = function(figure){
+        console.log(figure);
+        var draw2dFigure = eval("new "+figure.type+"();");
+        this.generateShape(draw2dFigure, figure.children);
+    }
+
+    //TODO: diese fabrik mit diesem weg funktioniert so nicht, da addFigure ein repaint macht und repaint
+    //      voraussetzt, dass das element bereits auf einem canvas gezeichnet ist
+    //      anderer weg: das menu triggert das zeichnen (macht ja auch sinn...) aus dem JSON -> drag and drop muss man dann neu schreiben
+    this.generateShape = function( parentShape, shape ){
+        $.each(shape, function(i,childShape){
+
+            var draw2dChildShape = eval("new "+childShape.type+"()");
+            parentShape.addFigure(draw2dChildShape, new draw2d.layout.locator.CenterLocator(parentShape));
+
+            if ( childShape.hasOwnProperty("children")){
+                that.generateShape(childShape, childShape.children);
+            }
+        });
+    }
+
+    this.convertSprayToDraw2dJSON = function(json){
+        $.each(json, function(name,sprayShape){
+            var draw2dShapeJSON = that.convertSprayToDraw2dShape(sprayShape.shapes);
+            obj = {};
+            obj[name] = {"type": "draw2d.Figure",
+                         "children": draw2dShapeJSON};
+            that.draw2dJSON.push( obj );
+        });
+    }
+
+    this.convertSprayToDraw2dShape = function(sprayShapes){
+        var result = [];
+        $.each(sprayShapes, function(i,sprayChildShape){
+            draw2dElementObj = that.convertSprayToDraw2dObj(sprayChildShape);
+            result.push( draw2dElementObj );
+            if ( sprayChildShape.shapes != "undefined" ){
+                draw2dElementObj["children"] = that.convertSprayToDraw2dShape(sprayChildShape.shapes);
+            }
+        });
+        if ( result.length > 0 ) return result;
+        return null;
+    }
+
+    this.convertSprayToDraw2dObj = function( sprayShape ){
+        var draw2dElementObj = {};
+        switch ( sprayShape.name ){
+            case "rounded-rectangle":
+                draw2dElementObj['type'] = "draw2d.shape.basic.Rectangle";
+                //TODO: default width?!
+                draw2dElementObj['width'] = sprayShape.size.width;
+                draw2dElementObj['height'] = sprayShape.size.height;
+                //TODO: no further specification for radius
+                draw2dElementObj['radius'] = sprayShape.curve.width;
+                break;
+            case "text":
+                draw2dElementObj['type'] = "draw2d.shape.basic.Label";
+                draw2dElementObj['text'] = sprayShape.id;
+                draw2dElementObj['width'] = sprayShape.size.width;
+                draw2dElementObj['height'] = sprayShape.size.height;
+                //TODO: default for horizontal/vertical
+                draw2dElementObj['cssClass'] = "label_"+ sprayShape.align.horizontal+"_"+sprayShape.align.vertical;
+            case "line":
+                break;
+            default:
+        }
+
+        if ( !sprayShape.hasOwnProperty('position') ){
+            draw2dElementObj['x'] = 0;
+            draw2dElementObj['y'] = 0;
+        }
+        else{
+            draw2dElementObj['x'] = sprayShape.position.x;
+            draw2dElementObj['y'] = sprayShape.position.y;
+        }
+
+        return draw2dElementObj;
+    }
+
+    this.initialize();
+}
--- sprayServer/public/javascripts/spray/menu.js
+++ sprayServer/public/javascripts/spray/menu.js
@@ -7,11 +7,11 @@ SPRAY.htwg.spray = SPRAY.htwg.spray || {};
 
 SPRAY.htwg.spray.Menu = function($){
 
-    var exampleJSON = [{ id:"elem1", type: "draw2d.shape.node.Start", title: "Start", attributes: ""},
+    /*var exampleJSON = [{ id:"elem1", type: "draw2d.shape.node.Start", title: "Start", attributes: ""},
                        { id:"elem2", type: "draw2d.shape.node.Between", title: "Between", attributes: ""},
                        { id:"elem3", type: "draw2d.shape.node.End", title: "Stop", attributes: ""},
                        { id:"elem4", type: "draw2d.shape.basic.Circle", title:"Circle", attributes: "10"},
-        { }];
+        { }];*/
 
     if ( jQuery('#menu').length === 0 ) {
         //no need to initialize
@@ -29,13 +29,17 @@ SPRAY.htwg.spray.Menu = function($){
 
     this.menu = jQuery("#menu");
 
-    this.initialize = function(){
-        jQuery.each(exampleJSON, function(i, item) {
+    this.buildMenu = function( shapes ){
+        $.each(shapes, function(i, item) {
             try{
                 canvasElem = $("<div style='width: 60px; height: 60px; display:none;' id='"+ item.id +"'></div>");
                 that.menu.append(canvasElem);
                 canvas = new draw2d.Canvas(item.id);
-                figure = eval("new "+item.type+"("+item.attributes+");");
+                console.log(item);
+
+                //TODO: hier zeichnen triggern!!!
+
+                figure = SPRAY.htwg.spray.Generator.shapeElements[item.name]();
                 figure.setDimension(30,30);
                 canvas.addFigure(figure,10,10);
                 shape = figure.getShapeElement();
@@ -51,7 +55,6 @@ SPRAY.htwg.spray.Menu = function($){
             }
             catch(e){console.log(e);}
         });
-    };
+    }
 
-    this.initialize();
 };
