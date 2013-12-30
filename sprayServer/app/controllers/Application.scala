4140c268d9b9ccd8d03e8f7275df09cb97b3dda3
ymc-sise <simon.schneeberger@ymc.ch>
2013-12-30 14:41:23 +0100

    removing test

--- sprayServer/app/controllers/Application.scala
+++ sprayServer/app/controllers/Application.scala
@@ -15,15 +15,15 @@ object Application extends Controller {
   
   def index = Action { implicit request =>
     // Test ECore
-    val factory: WebToEcoreFactory = WebToEcoreFactoryImpl.getInstance();
+    /*val factory: WebToEcoreFactory = WebToEcoreFactoryImpl.getInstance();
     factory.createEObject("Place");
     factory.createEObject("Transition");
     factory.createEObject("Transition");
     factory.createEObject("PTEdge", "Place", "Transition", 0, 1);
     factory.createEObject("PTEdge", "Place", "Transition", 0, 2);
-    factory.removeEObject("Transition",2);
+    factory.removeEObject("Transition",2);*/
     Ok(
-      views.html.index("Welcome to spray " + factory)
+      views.html.index("Welcome to spray ")
     )
   }
 
